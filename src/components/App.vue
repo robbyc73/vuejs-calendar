51512345aa02dac34fa657f63b7e1b2535e97b7d
robertcampbell <robcampbell73@gmail.com>
2018-11-18 21:37:38 +1100

    moment functions

---
+++ src/components/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div>Month: {{ currentMonth }}</div>
+        <div>Days on Month: {{ daysIMonth }}</div>
+        <div>Current Day: {{ currentDay }}</div>
+    </div>
+</template>
+<script>
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: 'Hello World 2',
+                //current month
+                currentMonth: this.$moment().month(),
+                //days on month
+                daysIMonth: this.$moment().daysInMonth(),
+
+
+                //current day
+                currentDay: this.$moment().dayOfYear()
+            };
+        },
+    }
+</script>
--- src/web.entry.js
+++ src/web.entry.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import './style.scss';
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
+Object.defineProperty(Vue.prototype,'$moment',{ get() {return this.$root.moment} });
 
+import App from './components/App.vue';
 new Vue({
   el: '#app',
-  data: {
-    msg: 'Hello World'
+    data: {
+      moment
+    },
+  components: {
+        App
   }
 });
